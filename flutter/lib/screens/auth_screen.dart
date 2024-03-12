83bc6bca5acc94e8996675836303ccee567fa6b2
Matthew Wong <matthewwong525@gmail.com>
2024-03-12 17:10:25 -0400

    add social logins to the ui

--- flutter/lib/screens/auth_screen.dart
+++ flutter/lib/screens/auth_screen.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:devtodollars/components/email_form.dart';
+import 'package:go_router/go_router.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
   @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  bool showEmailForm = false;
+  handleSignUpComplete(BuildContext context) {
+    if (!context.mounted) return;
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text("Check your Email!"),
+        content: const Text(
+            "We sent an email from pleasereply@devtodollars.com to verify your email"),
+        actions: [
+          TextButton(onPressed: context.pop, child: const Text("Ok Matt."))
+        ],
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    String? baseUrl = (kIsWeb) ? Uri.base.origin : null;
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            EmailForm(),
+            SupaSocialsAuth(
+              redirectUrl: baseUrl,
+              socialProviders: const [
+                OAuthProvider.google,
+                OAuthProvider.github,
+              ],
+              onSuccess: (_) {},
+            ),
+            const SizedBox(height: 16),
+            if (showEmailForm)
+              const Padding(
+                padding: EdgeInsets.only(bottom: 16),
+                child: Divider(),
+              ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: (showEmailForm)
+                  ? const EmailForm()
+                  : TextButton(
+                      onPressed: () => setState(() => showEmailForm = true),
+                      child: const Text("Continue with Email"),
+                    ),
+            ),
           ],
         ),
       ),
