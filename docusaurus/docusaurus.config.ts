be215897acebde392c7536ce1d4395d9fc13251b
Matthew Wong <matthewwong525@gmail.com>
2024-04-12 13:52:28 -0500

    make footer work

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -92,7 +92,7 @@ const config: Config = {
           items: [
             {
               label: 'Docs',
-              to: '/docs/intro',
+              to: '/',
             },
           ],
         },
