1c921e58981098c937f5ef4c0402d34fff29f30b
Matthew Wong <matthewwong525@gmail.com>
2024-04-13 17:53:08 -0500

    Docusaurus (#60)
    * init docusaurus
    
    * customize docs to my theme
    
    * add function to retrieve from remote repo
    
    * add docs from remote repo
    
    * update sidebar and footer
    
    * make footer work
    
    * fix some links
    
    * add basic docs to docusaurus to not make repo too big
    
    * add posthog docusaurus plugin
    
    * add back blog to docusaurus
    
    * add back simple landing and remove extra static assets
    
    * update config

---
+++ docusaurus/docusaurus.config.ts
@@ -0,0 +1,124 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'DevToDollars',
+  tagline: 'Helping Developers Become Founders',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://resources.devtodollars.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'devtodollars', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/devtodollars-social.png',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+    },
+    navbar: {
+      title: 'DevToDollars',
+      logo: {
+        alt: 'money bag',
+        src: 'img/devtodollars.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/devtodollars/startup-boilerplate',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Resources',
+          items: [
+            {
+              label: 'Docs',
+              to: '/docs',
+            },
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/s9DRKbwS',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/ithinkwong',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} DevToDollars`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+  plugins: [
+    [
+      "posthog-docusaurus",
+      {
+        apiKey: "phc_brFjanMrA2KXPYyvFjLdwJC9VJvl4VG7mG9yKqvgCCY",
+      },
+    ],
+  ],
+};
+
+export default config;
