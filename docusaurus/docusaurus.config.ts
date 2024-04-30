033affa561eac8e476ef1fa0891046509e105eec
Matthew Wong <matthewwong525@gmail.com>
2024-04-29 22:25:12 -0400

    update config + features home

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://resources.devtodollars.com',
+  url: 'https://devtodollars.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
--- docusaurus/src/components/features-home.tsx
+++ docusaurus/src/components/features-home.tsx
@@ -69,9 +69,6 @@ export default function FeaturesHome() {
                   </div>
                   <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow shrink-0 ml-3">
                     💰
-                    {/* <svg className="w-3 h-3 fill-current" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg"> */}
-                    {/*   <path d="M11.334 8.06a.5.5 0 00-.421-.237 6.023 6.023 0 01-5.905-6c0-.41.042-.82.125-1.221a.5.5 0 00-.614-.586 6 6 0 106.832 8.529.5.5 0 00-.017-.485z" fill="#191919" fillRule="nonzero" /> */}
-                    {/* </svg> */}
                   </div>
                 </button>
               </div>
