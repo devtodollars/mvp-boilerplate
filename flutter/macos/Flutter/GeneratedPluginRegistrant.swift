3386acbc0e783ef78ef0b9ae8b9087ee2e7ed513
Matthew Wong <matthewwong525@gmail.com>
2024-07-04 14:57:26 -0400

    update flutter to flutter 3.22 (#75)
    * update flutter
    
    * update flutter version

--- flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,19 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import google_sign_in_ios
 import path_provider_foundation
 import posthog_flutter
 import shared_preferences_foundation
+import sign_in_with_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PosthogFlutterPlugin.register(with: registry.registrar(forPlugin: "PosthogFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
