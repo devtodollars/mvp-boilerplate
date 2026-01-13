593c52ae6f0dd8be2b5cec5ef77b6d860e47d3f7
Amiralizim <aazimita@uwaterloo.ca>
2026-01-13 14:27:38 -0500

    [nextjs] fixed the getURL method for password redirect on production

--- nextjs/utils/helpers.ts
+++ nextjs/utils/helpers.ts
@@ -1,22 +1,23 @@
-export const getURL = (path: string = '') => {
+export const getURL = (path: string = "") => {
   // Check if NEXT_PUBLIC_SITE_URL is set and non-empty. Set this to your site URL in production env.
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL &&
-      process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
-      ? process.env.NEXT_PUBLIC_SITE_URL
-      : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-        process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
-        ? process.env.NEXT_PUBLIC_VERCEL_URL
-        : // If neither is set, default to localhost for local development.
-        'http://localhost:3000/';
+  let url: string;
+
+  if (process.env.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_SITE_URL.trim() !== "") {
+    url = process.env.NEXT_PUBLIC_SITE_URL;
+  } else if (process.env.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== "") {
+    // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
+    url = process.env.NEXT_PUBLIC_VERCEL_URL;
+  } else {
+    // If neither is set, default to localhost for local development.
+    url = "http://localhost:3000/";
+  }
 
   // Trim the URL and remove trailing slash if exists.
-  url = url.replace(/\/+$/, '');
+  url = url.replace(/\/+$/, "");
   // Make sure to include `https://` when not localhost.
-  url = url.includes('http') ? url : `https://${url}`;
+  url = url.includes("http") ? url : `https://${url}`;
   // Ensure path starts without a slash to avoid double slashes in the final URL.
-  path = path.replace(/^\/+/, '');
+  path = path.replace(/^\/+/, "");
 
   // Concatenate the URL and the path.
   return path ? `${url}/${path}` : url;
