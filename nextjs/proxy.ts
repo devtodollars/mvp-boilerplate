6e85edf95d130d5186bfcb2b223c9124c76ca169
Amirali Azimi <aazimita@uwaterloo.ca>
2026-01-13 13:55:46 -0500

    [nextjs] upgraded the boilerplate (#111)

--- nextjs/proxy.ts
+++ nextjs/proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from 'next/server';
 import { updateSession } from '@/utils/supabase/middleware';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   return await updateSession(request);
 }
 
